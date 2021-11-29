57eb776d033b44153106cc5dae09d820d44b3071
vesteny77 <yvesten_25e@icloud.com>
2021-11-29 12:01:55 -0500

    q1

--- phase3/queries_q1.sql
+++ phase3/queries_q1.sql
@@ -1,6 +1,18 @@
 -- How does a country’s living condition indicated by its GDP per capita affect its performance in the Olympics
--- indicated by the number of medals won by its athletes? And how does GDP affect the performance?
+-- indicated by the number of medals won by its athletes? And how does GDP affect performance of athletes?
 
+SET SEARCH_PATH TO Olympics;
+DROP TABLE IF EXISTS q1 CASCADE;
+
+CREATE TABLE q1 (
+    country VARCHAR(60),
+    gdp_per_capita FLOAT,
+    gdp FLOAT,
+    gold INT,
+    silver INT,
+    bronze INT,
+    total INT
+);
 -- Define Views
 DROP VIEW IF EXISTS IntermediateView CASCADE;
 
