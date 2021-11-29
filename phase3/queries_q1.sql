1b0c2cab82a290f3f8bf8e950819dc59e9512c89
vesteny77 <yvesten_25e@icloud.com>
2021-11-29 11:17:46 -0500

    start phase 3

---
+++ phase3/queries_q1.sql
@@ -0,0 +1,9 @@
+-- How does a country’s living condition indicated by its GDP per capita affect its performance in the Olympics
+-- indicated by the number of medals won by its athletes? And how does GDP affect the performance?
+
+-- Define Views
+DROP VIEW IF EXISTS IntermediateView CASCADE;
+
+CREATE VIEW IntermediateView;
+
+-- Output Results
---
+++ phase3/queries_q3.sql
@@ -0,0 +1,10 @@
+-- In which sport category (if exists), is each country
+-- 1) most likely to obtain more medals than other sports and
+-- 2) most likely to obtain more medals than other countries?
+
+-- Define Views
+DROP VIEW IF EXISTS IntermediateView CASCADE;
+
+CREATE VIEW IntermediateView;
+
+-- Output Results
