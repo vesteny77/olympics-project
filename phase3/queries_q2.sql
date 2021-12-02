49904ca7929a79319aef4a5b1d1c2a6c64f30245
vesteny77 <yvesten_25e@icloud.com>
2021-12-02 12:01:48 -0500

    modify

--- phase3/queries_q2.sql
+++ phase3/queries_q2.sql
@@ -2,9 +2,59 @@
 -- affect their likelihood to obtain a medal in their sports category?
 -- And which category of sport has the strongest such correlation?
 
--- Define Views
-DROP VIEW IF EXISTS IntermediateView CASCADE;
+SET SEARCH_PATH TO Olympics;
+DROP TABLE IF EXISTS q2 CASCADE;
 
-CREATE VIEW IntermediateView;
+CREATE TABLE q2 (
+    id INT,
+    age INT,
+    height FLOAT,
+    weight FLOAT,
+    sport VARCHAR(35),
+    gold INT,
+    silver INT,
+    bronze INT,
+    total INT
+);
+
+-- Define Views
+DROP VIEW IF EXISTS AthletePerformance CASCADE;
+DROP VIEW IF EXISTS AthletePerformanceAge CASCADE;
+DROP VIEW IF EXISTS AthletePerformanceHeight CASCADE;
+DROP VIEW IF EXISTS AthletePerformanceWeight CASCADE;
+
+CREATE VIEW AthletePerformance AS
+    SELECT id, (2016 - EXTRACT(YEAR FROM dob)) AS age, height, weight,
+        sport, gold, silver, bronze, gold + silver + bronze as total
+    FROM Athletes NATURAL JOIN Performance
+    ORDER BY sport, total DESC;
+
+CREATE VIEW AthletePerformanceAge AS
+    SELECT (2016 - EXTRACT(YEAR FROM dob)) AS age, sport,
+        sum(gold) as gold, sum(silver) as silver, sum(bronze) as bronze, 
+        sum(gold + silver + bronze) as total
+    FROM Athletes NATURAL JOIN Performance
+    GROUP BY age, sport
+    ORDER BY sport, total DESC;
+
+CREATE VIEW AthletePerformanceHeight AS
+    SELECT height, sport, sum(gold) as gold, sum(silver) as silver, sum(bronze) as bronze,
+        sum(gold + silver + bronze) as total
+    FROM Athletes NATURAL JOIN Performance
+    GROUP BY height, sport
+    ORDER BY sport, total DESC;
+
+CREATE VIEW AthletePerformanceWeight AS
+    SELECT weight, sport, sum(gold) as gold, sum(silver) as silver, sum(bronze) as bronze,
+        sum(gold + silver + bronze) as total
+    FROM Athletes NATURAL JOIN Performance
+    GROUP BY weight, sport
+    ORDER BY sport, total DESC;
 
 -- Output Results
+INSERT INTO q2
+    SELECT * FROM AthletePerformance;
+
+-- SELECT * FROM AthletePerformanceAge;
+-- SELECT * FROM AthletePerformanceHeight;
+SELECT * FROM AthletePerformanceWeight;
