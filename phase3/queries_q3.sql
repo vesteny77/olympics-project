603b227cde6ed3760ce77966a61c8cb56c8cf719
Angelina <angezhangn@gmail.com>
2021-12-02 00:28:06 -0500

    Update queries_q3.sql

--- phase3/queries_q3.sql
+++ phase3/queries_q3.sql
@@ -11,11 +11,35 @@ CREATE TABLE q3 (
     num_medals INT
 );
 
-
 -- Define Views
-DROP VIEW IF EXISTS IntermediateView CASCADE;
+DROP VIEW IF EXISTS totalpersport CASCADE;
+DROP VIEW IF EXISTS aggtps CASCADE;
+DROP VIEW IF EXISTS best CASCADE;
 
+-- Total number of medals for each sport for nation 
+CREATE VIEW totalpersport AS (
+    SELECT nationality, sport, (gold + silver + bronze) as total
+    FROM Performance
+    WHERE (gold + silver + bronze) <> 0
+);
 
-CREATE VIEW IntermediateView;
+-- Aggerated results from totalpersport 
+CREATE VIEW aggtps AS (
+    SELECT nationality, sport, sum(total) as total
+    FROM totalpersport
+    GROUP BY nationality, sport
+);
+
+-- The sport with top number of medal for each country
+CREATE VIEW best AS (
+    SELECT nationality, sport, total
+    FROM aggtps a1
+    WHERE total >= ALL(
+        SELECT total
+        FROM aggtps a2
+        WHERE a1.nationality = a2.nationality and a1.sport <> a2.sport
+        )
+);
 
 -- Output Results
+insert into q3 select * from best;
